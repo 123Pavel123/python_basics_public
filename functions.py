c8cc9c7a8c801b4eab89f1d788080597bf9fd006
hejjack <hejjack1@gmail.com>
2024-10-27 14:41:06 +0100

    update

--- functions.py
+++ functions.py
@@ -9,14 +9,22 @@ def greet(name, surname="", age=""):
 def f2():
     print("hello")
 
+def check_small_letter(character):
+    ord_char = ord(character)
+    if ord_char >= 97 and ord_char <= 122:
+        return True
+    else:
+        return False
+
+
 def main():
-    name = "Honza"
-    surname = "Votypka"
-    age = 22 # None
-
-    greet_honza = greet(name)
-    print("Funkce main: ", greet_honza)
-
+    # name = "Honza"
+    # surname = "Votypka"
+    # age = 22 # None
+    #
+    # greet_honza = greet(name)
+    # print("Funkce main: ", greet_honza)
+    print(check_small_letter("c"))
 
 if __name__ == "__main__":
     print("Zacatek programu, spoustim main()")
